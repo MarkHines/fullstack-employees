491ef30d6491719c58003d1a246a38f7ea6f5be7
Mark Hines <hines3521@gmail.com>
2025-06-27 10:17:57 -0400

    update post and delete routes

--- api/employees.js
+++ api/employees.js
@@ -41,14 +41,12 @@ router.get(`/:id`, async(request, response) => {
 router.delete(`/:id`, async(request, response) => {
   const { id } = request.params;
   const employeeId = Number(id)
-
-  if(employeeId < 0 || employeeId > 1000) {
-    response.status(400).send(`INCORRECT EMPLOYEE ID`)
-  }
-
   const singleEmployee = await deleteEmployee(employeeId);
 
-  if(!singleEmployee) {
+  // the regex /\D/.test() checks the id for non numeric characters
+  if(/\D/.test(id)) {
+    response.status(400).send(`INCORRECT EMPLOYEE ID`)
+  } else if(!singleEmployee) {
     response.status(404).send(`EMPLOYEE DOES NOT EXIST`);
   } else {
      response.status(204).send(`EMPLOYEE DELETED`);
@@ -58,20 +56,18 @@ router.delete(`/:id`, async(request, response) => {
 
 router.put(`/:id`, async(request, response) => {
   //console.log(request.body)
-  if(!request.body || !request.body.name || !request.body.birthday || !request.body.salary) {
-    response.status(400).send(`NO BODY PROVIDED`)
-  }
+  // the regex /\D/.test() checks the id for non numeric characters
 
   const { id } = request.params;
   const employeeId = Number(id)
-
-  if(employeeId < 0 || employeeId > 1000) {
-    response.status(400).send(`INCORRECT ID NUMBER`)
-  }
-  
   const updatedEmployee = await updateEmployee(employeeId);
 
-  if(!updatedEmployee) {
+
+  if(!request.body || !request.body.name || !request.body.birthday || !request.body.salary) {
+    response.status(400).send(`NO BODY PROVIDED`)
+  } else if(/\D/.test(id)) {
+    response.status(400).send(`INCORRECT ID NUMBER`)
+  } else if(!updatedEmployee) {
     response.status(404).send(`EMPLOYEE DOES NOT EXIST`);
   } else {
     response.status(200).send(updatedEmployee)
